db71cb92d35cc996e0a38d02e01d409eebbfd817
HungPhan <95025457+HungPhan200591@users.noreply.github.com>
2024-12-13 23:19:09 +0700

    Refactor code

--- src/main/java/lazy/demo/image_mngt_file_service/service/Test.java
+++ src/main/java/lazy/demo/image_mngt_file_service/service/Test.java
@@ -2,7 +2,12 @@ package lazy.demo.image_mngt_file_service.service;
 
 public class Test {
     public static void main(String[] args) {
-        long maxHeapSize = Runtime.getRuntime().maxMemory();
-        System.out.println("Maximum heap size: " + maxHeapSize / (1024 * 1024) + " MB");
+        long maxHeap = Runtime.getRuntime().maxMemory();
+        long totalHeap = Runtime.getRuntime().totalMemory();
+        long freeHeap = Runtime.getRuntime().freeMemory();
+
+        System.out.println("Max Heap (MB): " + maxHeap / 1024 / 1024);
+        System.out.println("Total Heap (MB): " + totalHeap / 1024 / 1024);
+        System.out.println("Free Heap (MB): " + freeHeap / 1024 / 1024);
     }
 }
